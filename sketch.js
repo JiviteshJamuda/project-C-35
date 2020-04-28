5dcdfde7b91feb3b7430dfa2bf33ba3fc9079b64
JiviteshJamuda <jivitesh2006@gmail.com>
2020-04-28 11:25:02 +0530

    survey

--- sketch.js
+++ sketch.js
@@ -1,7 +1,20 @@
+var DB,greeting,nameBar,emailIDBar,startButton;
+
 function setup() {
+  DB = firebase.database();
+  console.log(DB);
   createCanvas(500,500);
+
+  greeting = createElement('h2',"Welcome to my survey");
+  greeting.position(250,100);
+  nameBar = createInput("Name");
+  nameBar.position(250,200);
+  emailIDBar = createInput("email ID");
+  emailIDBar.position(250,250);
+  startButton = createButton("Start the survey");
+  startButton.position(400,300);
 }
 
 function draw() {
-  background(255,255,255);  
+  background(139,195,74);
 }
