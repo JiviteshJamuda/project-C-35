2399f893e596ee94539f74cb5a2821a972edc38c
JiviteshJamuda <jivitesh2006@gmail.com>
2020-04-29 10:20:20 +0530

    survey

--- sketch.js
+++ sketch.js
@@ -6,13 +6,13 @@ function setup() {
   createCanvas(500,500);
 
   greeting = createElement('h2',"Welcome to my survey");
-  greeting.position(250,100);
+  greeting.position(width/2+225,100);
   nameBar = createInput("Name");
-  nameBar.position(250,200);
+  nameBar.position(width/2+225,200);
   emailIDBar = createInput("email ID");
-  emailIDBar.position(250,250);
+  emailIDBar.position(width/2+225,250);
   startButton = createButton("Start the survey");
-  startButton.position(400,300);
+  startButton.position(500,300);
 }
 
 function draw() {
