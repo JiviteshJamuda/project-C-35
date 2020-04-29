9d317d9043caf9ce1404af9cbdbd884e3caf820a
JiviteshJamuda <jivitesh2006@gmail.com>
2020-04-29 20:23:53 +0530

    survey

--- sketch.js
+++ sketch.js
@@ -1,8 +1,17 @@
 var DB,greeting,nameBar,emailIDBar,startButton;
+var voterRef,voterCountRef;
+var voterCount=0,name=null,emailID=null;
+var name,emailID;
+var vote = [hi,there,mate];
+
+function updateCount(count){
+  DB.ref('/').update({
+    voterCount : count
+  });
+}
 
 function setup() {
   DB = firebase.database();
-  console.log(DB);
   createCanvas(500,500);
 
   greeting = createElement('h2',"Welcome to my survey");
@@ -13,8 +22,32 @@ function setup() {
   emailIDBar.position(width/2+225,250);
   startButton = createButton("Start the survey");
   startButton.position(500,300);
+
+  startButton.mousePressed(()=>{
+    name = nameBar.value();
+    emailID = emailIDBar.value();
+
+    voterCount+=1;
+    updateCount(voterCount);
+
+    voterRef = "voters/voter" + voterCount;
+    DB.ref(voterRef).set({
+      name:name,
+      emailID:emailID
+    });
+
+  })
+  
+  
 }
 
 function draw() {
   background(139,195,74);
+  voterCountRef = DB.ref('voterCount');
+  voterCountRef.on("value",(data)=>{
+    voterCount = data.val();
+  });
+  console.log(voterCount);
+
+
 }
