a0dad55c90728a0e1eef4a24a2294159dbd9b509
JiviteshJamuda <jivitesh2006@gmail.com>
2020-04-30 10:35:01 +0530

    survey

--- sketch.js
+++ sketch.js
@@ -1,8 +1,8 @@
-var DB,greeting,nameBar,emailIDBar,startButton;
-var voterRef,voterCountRef;
-var voterCount=0,name=null,emailID=null;
-var name,emailID;
-var vote = [hi,there,mate];
+var DB, greeting, nameBar, emailIDBar, startButton;
+var voterRef, voterCountRef;
+var voterCount=0, name = null, emailID = null, vote = null;
+var name, emailID, arr = [];
+var gameState = "formPage";
 
 function updateCount(count){
   DB.ref('/').update({
@@ -14,8 +14,11 @@ function setup() {
   DB = firebase.database();
   createCanvas(500,500);
 
-  greeting = createElement('h2',"Welcome to my survey");
-  greeting.position(width/2+225,100);
+  arr.push("yes");
+  vote = arr;
+
+  greeting = createElement('h1',"Welcome to my survey");
+  greeting.position(width/2+175,100);
   nameBar = createInput("Name");
   nameBar.position(width/2+225,200);
   emailIDBar = createInput("email ID");
@@ -23,21 +26,36 @@ function setup() {
   startButton = createButton("Start the survey");
   startButton.position(500,300);
 
+  
+
   startButton.mousePressed(()=>{
-    name = nameBar.value();
-    emailID = emailIDBar.value();
+    greeting.hide();
+    nameBar.hide();
+    emailIDBar.hide();
+    startButton.hide();
 
-    voterCount+=1;
-    updateCount(voterCount);
-
-    voterRef = "voters/voter" + voterCount;
-    DB.ref(voterRef).set({
-      name:name,
-      emailID:emailID
-    });
 
   })
   
+  if (gameState === "endPage"){
+    endButton = createButton("End the survey");
+    endButton.position(505,450);
+
+    endButton.mousePressed(()=>{
+      name = nameBar.value();
+      emailID = emailIDBar.value();
+  
+      voterCount+=1;
+      updateCount(voterCount);
+  
+      voterRef = "voters/voter" + voterCount;
+      DB.ref(voterRef).set({
+        name:name,
+        emailID:emailID,
+        vote:vote
+      });
+    });  
+  }
   
 }
 
@@ -48,6 +66,4 @@ function draw() {
     voterCount = data.val();
   });
   console.log(voterCount);
-
-
 }
